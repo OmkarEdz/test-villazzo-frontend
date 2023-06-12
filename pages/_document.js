66a351f9339e39a7eceb5f562b60c7b0e6882423
OmkarEdz <omkar.t@edreamz.in>
2023-06-12 20:38:46 +0530

    Added code all

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>          
           {/* eslint-disable-next-line */}
           <title> Miami Luxury Real Estate | Villazzo Realty </title>
+          <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
